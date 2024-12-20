219b5e694b62a2e2c4ab66fd5a6be812633eaa32
shinobiKoda <sirp2804@gmail.com>
2024-12-20 19:56:59 +0100

    feat: added planet card

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,17 @@ import React from "react";
 import "./index.css";
 import Background from "./components/Background";
 import Navbar from "./components/Navbar";
+import PlanetCard from "./components/PlanetCard";
+import { planets } from "./data/Planets";
 
 const App = () => {
   return (
     <div className="app-container">
       <Background>
         <Navbar />
+        {planets.map((planet) => (
+          <PlanetCard key={planet.name} planet={planet} />
+        ))}
       </Background>
     </div>
   );
--- src/data/Planets.js
+++ src/data/Planets.js
@@ -1,4 +1,4 @@
-const planets = [
+export const planets = [
   {
     name: "Mecury",
     rotationTime: "59 days",
@@ -8,17 +8,17 @@ const planets = [
     overview: {
       description:
         "Mercury is the first planet from the Sun and the smallest in the Solar System. In English, it is named after the ancient Roman god Mercurius (Mercury), god of commerce and communication, and the messenger of the gods. Mercury is classified as a terrestrial planet, with roughly the same surface gravity as Mars.",
-      image: "",
+      image: "/planet-mercury.svg",
     },
     internalStructure: {
       description:
         "Mercury appears to have a solid silicate crust and mantle overlying a solid, metallic outer core layer, a deeper liquid core layer, and a solid inner core.[28][29] The composition of the iron-rich core remains uncertain, but it likely contains nickel, silicon and perhaps sulfur and carbon, plus trace amounts of other elements.[30] The planet's density is the second highest in the Solar System at 5.427 g/cm3, only slightly less than Earth's density of 5.515 g/cm3.",
-      image: "",
+      image: "/planet-mecury-internal.svg",
     },
     surfaceGeology: {
       description:
         "Mercury's surface is similar in appearance to that of the Moon, showing extensive mare-like plains and heavy cratering, indicating that it has been geologically inactive for billions of years. It is more heterogeneous than the surface of Mars or the Moon, both of which contain significant stretches of similar geology, such as maria and plateaus.",
-      image: "",
+      image: "/geology-mecury.png",
     },
   },
   {
@@ -30,17 +30,17 @@ const planets = [
     overview: {
       description:
         "Venus is the second planet from the Sun. It is a terrestrial planet and is the closest in mass and size to its orbital neighbour Earth. Venus has by far the densest atmosphere of the terrestrial planets, composed mostly of carbon dioxide with a thick, global sulfuric acid cloud cover. At the surface it has a mean temperature of 737 K (464 °C; 867 °F) and a pressure 92 times that of Earth's at sea level.",
-      image: "",
+      image: "/planet-venus.svg",
     },
     internalStructure: {
       description:
         "Without data from reflection seismology or knowledge of its moment of inertia, little direct information is available about the internal structure and geochemistry of Venus. The similarity in size and density between Venus and Earth suggests that they share a similar internal structure: a core, mantle, and crust.",
-      image: "",
+      image: "/planet-venus-internal.svg",
     },
     surfaceGeology: {
       description:
         "Venus is a hot, dry world, with landscapes shaped by volcanoes and intense geological activity. Picture vast plains marked by lava flows, bordered by highlands and mountains. The surface of Venus is a scorching 464 °C on average, with a crushing air pressure 92 times bigger than we experience on Earth's surface. ",
-      image: "",
+      image: "/geology-venus.png",
     },
   },
   {
@@ -52,17 +52,17 @@ const planets = [
     overview: {
       description:
         "Third Planet from the Sun and the only known planet to harbor life. About 29.2% of Earth's surface is land with remaining 70.8% is covered with water. Earth's distance from the Sun, physical properties and geological history have allowed life to evolve and thrive.",
-      image: "",
+      image: "/planet-earth.svg",
     },
     internalStructure: {
       description:
         "Earth's interior, like that of the other terrestrial planets, is divided into layers by their chemical or physical(rheological) properties. The outer layer is a chemically distinct silicate solid crust, which is underlain by a highly viscous solid mantle.",
-      image: "",
+      image: "/planet-earth-internal.svg",
     },
     surfaceGeology: {
       description:
         "Most of Earth's surface is ocean water: 70.8% or 361 million km2 (139 million sq mi). This vast pool of salty water is often called the world ocean, and makes Earth with its dynamic hydrosphere a water world or ocean world. Indeed, in Earth's early history the ocean may have covered Earth completely.",
-      image: "",
+      image: "/geology-earth.png",
     },
   },
   {
@@ -74,17 +74,17 @@ const planets = [
     overview: {
       description:
         "Mars is the fourth planet from the Sun. The surface of Mars is orange-red because it is covered in iron(III) oxide dust, giving it the nickname 'the Red Planet'. Mars is among the brightest objects in Earth's sky, and its high-contrast albedo features have made it a common subject for telescope viewing. It is classified as a terrestrial planet and is the second smallest of the Solar System's planets with a diameter of 6,779 km (4,212 mi).",
-      image: "",
+      image: "/planet-mars.svg",
     },
     internalStructure: {
       description:
         "Like Earth, Mars is differentiated into a dense metallic core overlaid by less dense rocky layers.[45][46] The outermost layer is the crust, which is on average about 42-56 kilometres  thick, with a minimum thickness of 6 kilometres (3.7 mi) in Isidis Planitia, and a maximum thickness of 117 kilometres (73 mi) in the southern Tharsis plateau.",
-      image: "",
+      image: "/planet-mars-internal.svg",
     },
     surfaceGeology: {
       description:
         "Mars is a terrestrial planet with a surface that consists of minerals containing silicon and oxygen, metals, and other elements that typically make up rock. The Martian surface is primarily composed of tholeiitic basalt, although parts are more silica-rich than typical basalt and may be similar to andesitic rocks on Earth, or silica glass.",
-      image: "",
+      image: "/geology-mars.png",
     },
   },
   {
@@ -96,17 +96,17 @@ const planets = [
     overview: {
       description:
         "Jupiter is the fifth planet from the Sun and the largest in the Solar System. It is a gas giant with a mass more than 2.5 times that of all the other planets in the Solar System combined and slightly less than one-thousandth the mass of the Sun. Its diameter is eleven times that of Earth, and a tenth that of the Sun.",
-      image: "",
+      image: "/planet-jupiter.svg",
     },
     internalStructure: {
       description:
         "Before the early 21st century, most scientists proposed one of two scenarios for the formation of Jupiter. If the planet accreted first as a solid body, it would consist of a dense core, a surrounding layer of fluid metallic hydrogen (with some helium) extending outward to about 80% of the radius of the planet,and an outer atmosphere consisting primarily of molecular hydrogen.",
-      image: "",
+      image: "/planet-jupiter-internal.svg",
     },
     surfaceGeology: {
       description:
         "As a gas giant, Jupiter doesn't have a true surface. The planet is mostly swirling gases and liquids. While a spacecraft would have nowhere to land on Jupiter, it wouldn't be able to fly through unscathed either",
-      image: "",
+      image: "/geology-jupiter.png",
     },
   },
   {
@@ -118,17 +118,17 @@ const planets = [
     overview: {
       description:
         "Saturn is the sixth planet from the Sun and the second largest in the Solar System, after Jupiter. It is a gas giant, with an average radius of about nine times that of Earth. It has an eighth the average density of Earth, but is over 95 times more massive.",
-      image: "",
+      image: "/planet-saturn.svg",
     },
     internalStructure: {
       description:
         "Despite consisting mostly of hydrogen and helium, most of Saturn's mass is not in the gas phase, because hydrogen becomes a non-ideal liquid when the density is above 0.01 g/cm3, which is reached at a radius containing 99.9% of Saturn's mass",
-      image: "",
+      image: "/planet-saturn-internal.svg",
     },
     surfaceGeology: {
       description:
         "As a gas giant, Saturn doesn't have a true surface. The planet is mostly swirling gases and liquids deeper down.",
-      image: "",
+      image: "/geology-saturn.png",
     },
   },
   {
@@ -140,17 +140,17 @@ const planets = [
     overview: {
       description:
         "Uranus is the seventh planet from the Sun. It is a gaseous cyan-coloured ice giant. Most of the planet is made of water, ammonia, and methane in a supercritical phase of matter, which astronomy calls 'ice' or volatiles. The planet's atmosphere has a complex layered cloud structure and has the lowest minimum temperature of all the Solar System's planets.",
-      image: "",
+      image: "/planet-uranus.svg",
     },
     internalStructure: {
       description:
         "Uranus's mass is roughly 14.5 times that of Earth, making it the least massive of the giant planets. Its diameter is slightly larger than Neptune's at roughly four times that of Earth. A resulting density of 1.27 g/cm3 makes Uranus the second least dense planet, after Saturn. This value indicates that it is made primarily of various ices, such as water, ammonia, and methane.",
-      image: "",
+      image: "/planet-uranus-internal.svg",
     },
     surfaceGeology: {
       description:
         "As an ice giant, Uranus doesn't have a true surface. The planet is mostly swirling fluids. While a spacecraft would have nowhere to land on Uranus, it wouldn't be able to fly through its atmosphere unscathed either.",
-      image: "",
+      image: "/geology-uranus.png",
     },
   },
   {
@@ -162,17 +162,17 @@ const planets = [
     overview: {
       description:
         "Neptune is the eighth and farthest known planet from the Sun. It is the fourth-largest planet in the Solar System by diameter, the third-most-massive planet, and the densest giant planet. It is 17 times the mass of Earth",
-      image: "",
+      image: "/planet-neptune.svg",
     },
     internalStructure: {
       description:
         "Neptune's internal structure resembles that of Uranus. Its atmosphere forms about 5 to 10% of its mass and extends perhaps 10 to 20% of the way towards the core. Pressure in the atmosphere reaches about 10 GPa, or about 105 atmospheres. Increasing concentrations of methane, ammonia and water are found in the lower regions of the atmosphere.",
-      image: "",
+      image: "/planet-neptune-internal.svg",
     },
     surfaceGeology: {
       description:
         "Neptune does not have a solid surface. Its atmosphere (made up mostly of hydrogen, helium, and methane) extends to great depths, gradually merging into water and other melted ices over a heavier, solid core with about the same mass as Earth.",
-      image: "",
+      image: "/geology-neptune.svg",
     },
   },
 ];
